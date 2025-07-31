4e21f570916b1705e69665857fa0c286ce6bb85e
boot <bootovy@proton.me>
2025-07-31 23:56:37 +0200

    Add Fast Forward functionality in debug builds

---
+++ scripts/menus/fast_forward_indicator.gd
@@ -0,0 +1,33 @@
+# Copyright (C) 2025 Libre Nikki Developers.
+#
+# This file is part of Libre Nikki.
+#
+# Libre Nikki is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Libre Nikki is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Libre Nikki. If not, see <https://www.gnu.org/licenses/>.
+
+extends PanelContainer
+
+func _ready() -> void:
+	hide()
+
+func _process(delta: float) -> void:
+	if OS.is_debug_build():
+		if Input.is_key_pressed(KEY_F):
+			if Engine.time_scale == 1.0:
+				show()
+
+			Engine.time_scale = 3.0
+		else:
+			if Engine.time_scale == 3.0:
+				hide()
+
+			Engine.time_scale = 1.0
