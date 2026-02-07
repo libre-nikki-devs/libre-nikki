b547a53b1a32dfcd6c8c5cfd9a18a95db033a6ae
boot <bootovy@proton.me>
2026-02-07 16:12:07 +0100

    Debugtsuki: Inherit Wanderer script

--- scripts/characters/debugtsuki.gd
+++ scripts/characters/debugtsuki.gd
@@ -14,58 +14,13 @@
 # You should have received a copy of the GNU General Public License along with
 # Libre Nikki. If not, see <https://www.gnu.org/licenses/>.
 
-extends YumeHumanoid
-
-## A simple, non-playable character that walks around aimlessly.
-
-## Amount of time (in seconds) to wait before attempting to move a
-## randomly-picked direction.
-@export var wait_time: float = 2.0
-
-var wait_timer: float = RandomNumberGenerator.new().randf_range(0.0, wait_time)
-
-signal waited
+extends "res://scripts/ai/YumeHumanoid/wanderer.gd"
 
 func _init() -> void:
 	super()
 
 	if not is_connected("body_interacted", _on_body_interacted):
 		connect("body_interacted", _on_body_interacted)
-
-func _ready() -> void:
-	_move_loop()
-
-func _physics_process(delta: float) -> void:
-	wait_timer += delta
-
-	if wait_timer >= wait_time:
-		wait_timer = 0.0
-		waited.emit()
-
-func _move_loop():
-	while true:
-		var available_directions: Array[DIRECTION]
-
-		for direction in DIRECTION.values():
-			available_directions.append(direction)
-
-		await waited
-
-		if not is_busy:
-			var can_move: bool = false
-			var picked_direction: DIRECTION = DIRECTION.LEFT
-
-			while not (available_directions.is_empty() or can_move):
-				picked_direction = available_directions.pick_random()
-
-				if is_colliding(picked_direction):
-					available_directions.erase(picked_direction)
-				else:
-					can_move = true
-
-			if can_move:
-				facing = picked_direction
-				move(picked_direction)
 
 func _on_body_interacted(body: Node2D) -> void:
 	if not is_busy:
