c4a2c369cc52112dcc697b202dbb13aa28e38afe
boot <bootovy@proton.me>
2026-03-11 14:54:18 +0100

    Add Pushable script

---
+++ scripts/props/pushable.gd
@@ -0,0 +1,29 @@
+# Copyright (C) 2026 Libre Nikki Developers.
+#
+# This file is part of Libre Nikki.
+#
+# Libre Nikki is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Libre Nikki is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Libre Nikki. If not, see <https://www.gnu.org/licenses/>.
+
+extends YumeCharacter
+
+func _init() -> void:
+	super()
+	body_touched.connect(_on_body_touched)
+
+func _on_body_touched(body: Node2D):
+	if body is YumeCharacter and not is_busy:
+		var direction: DIRECTION = body.face(global_position)
+
+		if not is_colliding(direction):
+			move(direction)
+			body.move(direction)
