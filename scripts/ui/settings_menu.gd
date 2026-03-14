149f33a9ba25d2e2c483d2d3033fbae30ceb522d
boot <bootovy@proton.me>
2026-03-14 16:25:15 +0100

    SettingsMenu: Move Updates to SideMenu

--- scripts/ui/settings_menu.gd
+++ scripts/ui/settings_menu.gd
@@ -16,12 +16,12 @@
 
 extends YumeMenu
 
-@onready var about_button := $SidePanelContainer/SideVBoxContainer/AboutButton
-@onready var about_menu := $MainPanelContainer/AboutVBoxContainer
+@onready var licenses_button := $SidePanelContainer/SideVBoxContainer/LicensesButton
+@onready var licenses_menu := $MainPanelContainer/LicensesVBoxContainer
 @onready var side_menu := $SidePanelContainer/SideVBoxContainer
 
 func _ready() -> void:
-	about_menu.show()
+	licenses_menu.show()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_go_back"):
@@ -34,19 +34,19 @@ func _input(event: InputEvent) -> void:
 
 		if focus_owner:
 			match focus_owner.get_parent():
-				about_menu:
-					about_button.grab_focus()
+				licenses_menu:
+					licenses_button.grab_focus()
 
 				side_menu:
 					close()
 
 func _get_focus_grabber() -> Control:
-	return about_button
+	return licenses_button
 
-func _on_about_button_pressed() -> void:
-	about_menu.show()
+func _on_licenses_button_pressed() -> void:
+	licenses_menu.show()
 
-	for child: Node in about_menu.get_children():
+	for child: Node in licenses_menu.get_children():
 		if child is Button:
 			child.grab_focus()
 			break
@@ -57,5 +57,5 @@ func _on_cc_button_pressed() -> void:
 func _on_gpl_button_pressed() -> void:
 	open_submenu("res://scenes/ui/license_menu.tscn", { "license_text": preload("res://scenes/ui/license_menu.tscn").instantiate().GPL_FORMATTED })
 
-func _on_update_button_pressed() -> void:
+func _on_updates_button_pressed() -> void:
 	open_submenu("res://scenes/ui/update_checker.tscn")
